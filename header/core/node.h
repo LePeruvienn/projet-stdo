2873d01d1a9b10ea635bd81222c31be61d1f8a5f
Eva GENTILHOMME <ethan.gentilhomme@ensiie.eu>
2026-06-21 19:11:10 +0200

    feat: node implementation

--- header/core/node.h
+++ header/core/node.h
@@ -1,11 +1,41 @@
 #ifndef NODE_H
 #define NODE_H
 
+/**
+ * \brief Represents a list of edges originating from
+ * the same node, with the destination and the distance from the node. 
+ */
 typedef struct s_node node;
 
-node    *node_new();
+/**
+ * \brief Creates a new node.
+ * \attention The node muist be freed with node_free()
+ * \return New hashmap
+ */
+node    *node_new       ();
 
-void    node_free(node *n);
+/**
+ * \brief Add an edge (destination, distance) to the node
+ * \param n Source node
+ * \param dest Destination node
+ * \param distance Distance from the node
+ */
+void    node_add_edge  (node *n, int dest, float distance);
+
+/**
+ * \brief Get the distance to an other node if there's an edge.
+ * \param n The node (source)
+ * \param dest The other node (destination)
+ * \return The distance if there is an edge, and -1.0 if there's no edge
+ *      between the two nodes.
+ */
+float   node_get_edge  (node *n, int dest);
+
+/**
+ * \brief Frees a node
+ * \param n The node to be freed
+ */
+void    node_free       (node *n);
 
 #endif // NODE_H
 
--- src/core/node.c
+++ src/core/node.c
@@ -1,19 +1,96 @@
 #include "core/node.h"
-
 #include <stdlib.h>
 
-struct s_node
+typedef	struct s_edge 
 {
-	int foo;
+	int				dest;
+	float			distance;
+	struct s_edge	*next;
+} nd_edge;
+
+struct	s_node
+{
+	nd_edge			*first;		
 };
 
 node    *node_new()
 {
 	node* n = malloc(sizeof(struct s_node));
+	n->first = NULL;
 	return n;
 }
 
-void    node_free(node *n)
+float	node_get_edge(node *n, int dest)
 {
+	nd_edge *current = n->first;
+
+	while (current != NULL)
+	{
+		if (current->dest == dest)
+		{
+			return current->distance;
+		}
+		else
+		{
+			current = current->next;
+		}
+	}
+
+	return -1.f;
+}
+
+static nd_edge *__node_new_edge(int dest, float distance)
+{
+	nd_edge *edge = malloc(sizeof(nd_edge));
+
+	edge->dest = dest;
+	edge->distance = distance;
+	edge->next = NULL;
+
+	return edge;
+}
+
+void	node_add_edge(node *n, int dest, float distance)
+{
+	if (n->first == NULL)
+	{
+		n->first = __node_new_edge(dest, distance); 
+	}
+	else
+	{
+		nd_edge *current = n->first;
+		while (current->next != NULL) {
+			// if it already exists, we just replace the distance and return
+			if (current->dest == dest) 
+			{
+				current->distance = distance;
+				return;
+			}
+			current = current->next;
+
+		}
+		// we're at the last edge, so we're inserting the new edge
+		current->next = __node_new_edge(dest, distance);
+	}
+}
+
+void	node_free(node *n)
+{
+	nd_edge *current = n->first;
+	
+	while (current != NULL)
+	{
+		if (current->next != NULL)
+		{
+			nd_edge *to_remove = current;
+			current = to_remove->next;
+			free(to_remove);
+		}
+		else
+		{
+			free(current);
+		}
+	}
+
 	free(n);
 }
