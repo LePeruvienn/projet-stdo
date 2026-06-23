28db0f61e5c4522b18c626d23c0738d3434d7472
Eva G. <orakle@tutamail.com>
2026-06-23 18:29:37 +0200

    feat: heap specification

---
+++ header/core/heap.h
@@ -0,0 +1,20 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include "core/edge.h"
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct s_heap heap;
+
+heap    *heap_new       (size_t initial_size);
+
+void    heap_free       (heap *h);
+
+bool    heap_is_empty   (heap *h);
+
+void    heap_insert     (heap *h, edge *value);
+
+edge    *heap_pop       (heap *h);
+
+#endif // HEAP_H
